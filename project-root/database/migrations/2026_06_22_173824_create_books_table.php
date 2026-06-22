efd6045515d69b892749038d288fe8192d4b9130
chirzin633 <chirzin.attabiq@gmail.com>
2026-06-23 01:42:00 +0700

    init filament and migration

---
+++ project-root/database/migrations/2026_06_22_173824_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->integer('stock');
+            $table->year('published_year')->nullable();
+            $table->string('cover')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ project-root/database/migrations/2026_06_22_173854_create_loans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('member_id');
+            $table->string('member_code');
+            $table->date('loan_date');
+            $table->date('due_date');
+            $table->date('return_date')->nullable();
+            $table->decimal('fine', 10, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
