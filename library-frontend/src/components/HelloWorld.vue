825b657a5de933a9edd009783f06ec6d16168b22
chirzin633 <chirzin.attabiq@gmail.com>
2026-06-26 13:23:25 +0700

    init vue for frontend

---
+++ library-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const tambah = () => {
+  count.value++;
+};
+
+const kurang = () => {
+  if (count.value === 0) {
+    return;
+  }
+  count.value--;
+};
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <h1 class="font-bold text-teal-600 text-3xl text-center uppercase">
+      Ini halaman front end
+    </h1>
+
+    <p class="font-bold text-center">
+      Count : <span class="text-red-400">{{ count }}</span>
+    </p>
+
+    <div class="flex justify-center gap-2">
+      <button @click="tambah" class="bg-slate-400 px-3 py-1 w-10">+</button>
+      <button @click="kurang" class="bg-slate-400 px-3 py-1 w-10">-</button>
+    </div>
+  </div>
+</template>
